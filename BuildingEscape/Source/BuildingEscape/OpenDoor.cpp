f369e7c5531cfe460c8b46a98d0e50a97ce4a795
JOEKIM\joeki <joe.kim509@gmail.com>
2017-08-25 22:50:17 -0500

    Built the walls on the map.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
-
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -19,7 +19,9 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	AActor *Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
 	
 }
 
